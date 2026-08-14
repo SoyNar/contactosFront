ed8514cbe501e3d96cafde9c2025a3ec23a88686
SoyNar <narciris.tech@gmail.com>
2026-08-14 18:54:45 -0500

    wip: antes de actualizar angular

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {path:'', redirectTo: 'contacts', pathMatch: 'full'},
+    {path:'contacts', loadComponent: () => import('./pages/contacts/contacts.component').then(m => m.ContactsComponent)},
+    {path: '**', redirectTo: 'contacts'}
+];
